cf79f2b4135c3e2fee2f87775454bcb7d37aae24
Amber <110812972+Msambere@users.noreply.github.com>
2024-12-19 09:27:03 -0600

    Add css to new task form

--- src/components/NewTaskForm.jsx
+++ src/components/NewTaskForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import './NewTaskForm.css';
 
 const NewTaskForm = ({submitNewTask}) => {
   const [taskDetails, setTaskDetails] = useState({title: '', description: ''});
